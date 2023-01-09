95dedf8e366b3b6a154bceae7009ed8f178300c0
IvailoTonchev <ivcho465@gmail.com>
2023-01-09 10:20:31 +0200

    edit

---
+++ Controllers/EstateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiplomenProekt.Controllers
+{
+    public class EstateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
