c60221545e260893116e1278875b34e4c7127bf9
IvailoTonchev <ivcho465@gmail.com>
2023-02-10 12:49:42 +0200

    appuser

---
+++ Data/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace DiplomenProekt.Data.Models
+{
+    public class AppUser
+    {
+        public string UserName { get; internal set; }
+        public string Email { get; internal set; }
+    }
+}
